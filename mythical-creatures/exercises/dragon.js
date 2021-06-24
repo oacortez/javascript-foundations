da865e1fae7034fe30129744f0c92a0e782f7b85
Oscar Cortez <oacortez98@gmail.com>
2021-06-24 11:24:49 -0600

    Completed dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,27 @@
+class Dragon {
+  constructor(dragonName, owner) {
+    this.name = dragonName;
+    this.rider = owner;
+    this.hungry = true;
+    this.food = 0;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat(){
+    this.food += 1;
+    if(this.food >= 3) {
+      return this.hungry = false;
+      }
+    }
+  }
 
+
+
+
+
+
+
+
+
+module.exports = Dragon;
