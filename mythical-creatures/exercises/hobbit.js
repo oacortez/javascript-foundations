01121fd2ee5affc314f63c1c0edfb7b3f18c41b5
Oscar Cortez <oacortez98@gmail.com>
2021-06-24 17:41:31 -0600

    Completed hobbit test

---
+++ mythical-creatures/exercises/hobbit.js
@@ -0,0 +1,35 @@
+class Hobbit {
+  constructor(hobbitName) {
+    this.name = hobbitName.name;
+    this.age = 0;
+    this.adult = false;
+    this.old = false;
+    this.hasRing = false;
+  }
+  celebrateBirthday() {
+    this.age += 1;
+    if(this.age >= 33) {
+       this.adult = true;
+    }
+    if(this.age >= 101) {
+       this.old = true;
+    }
+  }
+  getRing() {
+    if(this.name === 'Frodo') {
+      this.hasRing = true;
+      return "Here is the ring!"
+    } else {
+      return "You can\'t have it!"
+    }
+  }
+}
+
+
+
+
+
+
+
+
+module.exports = Hobbit;
