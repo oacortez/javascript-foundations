d5d52343ebc013fae1b94e08c21dc805a61a84e1
Oscar Cortez <oacortez98@gmail.com>
2021-07-21 19:41:49 -0600

    Complete lunchbox

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,26 @@
 class LunchBox {
-
+  constructor(owner) {
+    this.owner = owner.owner;
+    this.material = owner.madeOf;
+    this.shape = owner.shape;
+    this.color = owner.color;
+    this.snacks = [];
+  }
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    if(snack.isInLunchBox === false) {
+      return snack.isInLunchBox = true;
+    }
+  }
+  findHealthySnacks() {
+    var healthySnacksSelection = [];
+    for(var i = 0; i < this.snacks.length; i++) {
+      if(this.snacks[i].checkForHealthy()) {
+         healthySnacksSelection.push(this.snacks[i].type)
+       }
+    }
+    return healthySnacksSelection
+  }
 }
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,25 @@
 class Snack {
+  constructor(snackType) {
+    this.deliciousLevel = "extra";
+    this.type = snackType;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+  }
+  getEaten() {
+    this.amount -= 10;
+    if(this.amount <= 20) {
+      this.cuttingItClose = true;
+    }
+  }
+  checkForHealthy() {
+    if(this.type.toLowerCase().includes('fruit')){
+      return true;
+    } else {
+      return false;
+      }
+    }
+  }
 
-}
 
 module.exports = Snack;
